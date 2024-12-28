607f1ac822e4916925b0777c8d6155168fd05fdc
amnakamran1 <Amnakamran2024@gmail.com>
2024-12-27 23:53:26 -0800

    form

---
+++ Aspform/Aspform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aspform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Aspform/Aspform/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace Aspform.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public Gender gender { get; set; }
+    }
+    public enum Gender
+    {
+        Male, Female
+
+    }
+}
