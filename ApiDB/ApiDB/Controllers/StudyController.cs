5c6745ba1fdcead0490c08e1b40fde742c344bc7
amnakamran1 <Amnakamran2024@gmail.com>
2025-01-01 22:13:38 -0800

    API

---
+++ ApiDB/ApiDB/Controllers/StudyController.cs
@@ -0,0 +1,24 @@
+﻿using ApiDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ApiDB.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudyController : ControllerBase
+    {
+        [HttpGet("students")]
+        public IActionResult GetStudents()
+        {
+            var students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Amna", Description = "good" },
+                new Student { Id = 2, Name = "Tabish", Description = "good" },
+                new Student { Id = 3, Name = "Smar", Description = "good" }
+            };
+
+            return Ok(students);
+        }
+    }
+}
